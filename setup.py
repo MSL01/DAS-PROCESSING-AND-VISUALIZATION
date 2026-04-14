6e478c2b988803a8a38571ceb637e4bb43fd20fe
MSL01 <100708772+MSL01@users.noreply.github.com>
2026-04-13 21:36:04 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="das_nidf",
+    version="1.1",
+    author="Miguel A Saavedra L",
+    author_email="miguel.lozano@lps.ufrj.br",
+    description="Distributed Acoustic Sensing  Processing and Data Visualization Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MSL01/DAS-PROCESSING-AND-VISUALIZATION.git",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy>=1.20.0",
+        "scipy>=1.7.0",
+        "h5py>=3.0.0",
+        "holoviews>=1.15.0",
+        "datashader>=0.14.0",
+        "bokeh>=2.4.0",
+        "scikit-image>=0.19.0",
+    ],
+    extras_require={
+        "dev": ["pytest>=6.0", "jupyter"],
+        "full": ["matplotlib>=3.4.0", "tqdm>=4.62.0"],
+    },
+)
